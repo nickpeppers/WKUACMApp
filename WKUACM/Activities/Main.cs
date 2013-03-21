f07230e0843811c27f1bec46981438505adbbb4e
Nick Peppers <nick.peppers@hitcents.com>
2013-03-21 17:13:47 -0500

    Added start of the ACM app
    got splash screen going

---
+++ WKUACM/Activities/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace WKUACM
+{
+    [Activity(Label = "Main Activity")]
+    public class Main : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+        }
+    }
+}
+
